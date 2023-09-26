59c343d317fc20967bef28716afe5ae8dd5ba8ef
aprilianticp <aprilputriapril27@gmail.com>
2023-09-26 15:50:24 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// Scroll smooth effect for navigation links
+document.addEventListener("DOMContentLoaded", function() {
+  const navLinks = document.querySelectorAll("nav ul li a");
+  
+  for (const
